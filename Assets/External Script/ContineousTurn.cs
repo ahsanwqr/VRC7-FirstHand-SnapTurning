c3a4cf72954fc6bc53db4f39a0d30f2f73f70589
Ahsan <ahsanwqr@gmail.com>
2023-03-02 16:43:39 +0500

    add continuous turn.

--- Assets/External Script/ContineousTurn.cs
+++ Assets/External Script/ContineousTurn.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ContineousTurn : MonoBehaviour
@@ -9,14 +7,23 @@ public class ContineousTurn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstickRight))
-        {
-            gameObject.transform.Translate(Vector3.forward * PlayerMovementSpeed * Time.deltaTime, Space.World);
-        }
+        /* if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstickRight))
+         {
+             gameObject.transform.Translate(Vector3.right * PlayerMovementSpeed * Time.deltaTime, Space.World);
+         }
 
-        if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstickLeft))
+         if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstickLeft))
+         {
+             gameObject.transform.Translate(Vector3.left * PlayerMovementSpeed * Time.deltaTime, Space.World);
+         }*/
+
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight))
         {
-            gameObject.transform.Translate(Vector3.back * PlayerMovementSpeed * Time.deltaTime, Space.World);
+            transform.Rotate(Vector3.up * PlayerMovementSpeed * Time.deltaTime);
+        }
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft))
+        {
+            transform.Rotate(Vector3.down * PlayerMovementSpeed * Time.deltaTime);
         }
     }
 }
