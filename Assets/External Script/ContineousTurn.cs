ff232cff30af2cb60a8f0b50d0d8151c8e885023
Ahsan <ahsanwqr@gmail.com>
2023-03-03 16:09:04 +0500

    contineous Turn

--- Assets/External Script/ContineousTurn.cs
+++ Assets/External Script/ContineousTurn.cs
@@ -7,16 +7,6 @@ public class ContineousTurn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /* if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstickRight))
-         {
-             gameObject.transform.Translate(Vector3.right * PlayerMovementSpeed * Time.deltaTime, Space.World);
-         }
-
-         if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstickLeft))
-         {
-             gameObject.transform.Translate(Vector3.left * PlayerMovementSpeed * Time.deltaTime, Space.World);
-         }*/
-
         if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight))
         {
             transform.Rotate(Vector3.up * PlayerMovementSpeed * Time.deltaTime);
